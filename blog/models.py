43e139bd039c35e6b4dff94cb98bd1fd1c961ba8
max <maxandreev27@gmail.com>
2026-05-15 19:17:21 +0300

    Add adminpanel, blog urls, views, templates

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     """Post model for blog"""
 
@@ -22,6 +27,9 @@ class Post(models.Model):
     status = models.CharField(
         max_length=2, choices=Status.choices, default=Status.DRAFT
     )
+
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
 
     class Meta:
         ordering = ["-publish"]
